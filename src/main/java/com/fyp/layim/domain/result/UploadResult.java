417100e3ff659c4a7fe2d89ffed4897aa3531e5a
fanpan26 <fanpan26@126.com>
2017-11-30 15:04:33 +0800

    add upload file methods

---
+++ src/main/java/com/fyp/layim/domain/result/UploadResult.java
@@ -0,0 +1,23 @@
+package com.fyp.layim.domain.result;
+
+public class UploadResult {
+    private String src;
+
+    public String getSrc() {
+        return src;
+    }
+
+    public void setSrc(String src) {
+        this.src = src;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+}
---
+++ src/main/java/com/fyp/layim/service/UploadService.java
@@ -0,0 +1,44 @@
+package com.fyp.layim.service;
+
+import com.fyp.layim.common.util.ResultUtil;
+import com.fyp.layim.domain.result.JsonResult;
+import com.fyp.layim.domain.result.LAYIM_ENUM;
+import com.fyp.layim.domain.result.UploadResult;
+import org.apache.commons.lang3.StringUtils;
+import org.hibernate.id.GUIDGenerator;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@Service
+public class UploadService {
+    /**
+     *  上传文件
+     * */
+    public JsonResult uploadFile(MultipartFile file,String fileDirPath) throws IOException{
+        if(!file.isEmpty()){
+            String fileName = file.getOriginalFilename();
+            String extensionName = StringUtils.substringAfter(fileName,".");
+            //生成新的guid的文件名
+            String newFileName = UUID.randomUUID()+ "."+extensionName;
+            Path filePath = Paths.get(fileDirPath, newFileName);
+            //保存到响应的路径下
+            Files.copy(file.getInputStream(), filePath);
+
+            UploadResult uploadResult = new UploadResult();
+            uploadResult.setSrc("/upload/" + newFileName);
+            uploadResult.setName(fileName);
+
+            return ResultUtil.success(uploadResult);
+        }else{
+            return ResultUtil.fail(LAYIM_ENUM.NO_FILE);
+        }
+    }
+}

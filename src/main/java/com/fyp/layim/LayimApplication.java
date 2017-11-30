55c31cacc8c7dfd7449f987f7a3ae3f05fe8482c
fanpan26 <fanpan26@126.com>
2017-11-30 15:04:14 +0800

    remove useless codes

--- src/main/java/com/fyp/layim/LayimApplication.java
+++ src/main/java/com/fyp/layim/LayimApplication.java
@@ -12,11 +12,5 @@ import org.springframework.context.ApplicationContext;
 public class LayimApplication {
     public static void main(String[] args) {
        ApplicationContext applicationContext = SpringApplication.run(LayimApplication.class, args);
-
-//        String[] beanNames =  applicationContext.getBeanDefinitionNames();
-//        System.out.println("beanNames个数："+beanNames.length);
-//        for(String bn:beanNames){
-//            System.out.println(bn);
-//        }
     }
 }

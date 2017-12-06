c49b724d85c80220c3e44dd4226492fbce033d4b
fanpan26 <fanpan26@126.com>
2017-12-06 23:59:27 +0800

    增加推送方法

--- src/main/java/com/fyp/layim/im/common/util/SpringUtil.java
+++ src/main/java/com/fyp/layim/im/common/util/SpringUtil.java
@@ -1,12 +1,17 @@
 package com.fyp.layim.im.common.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ApplicationEvent;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SpringUtil implements ApplicationContextAware {
+
+    private static Logger logger = LoggerFactory.getLogger(SpringUtil.class);
 
     private static ApplicationContext applicationContext = null;
     private static SpringUtil Instance = null;
@@ -24,5 +29,13 @@ public class SpringUtil implements ApplicationContextAware {
 
     public synchronized static Object getBean(String beanName) {
         return applicationContext.getBean(beanName);
+    }
+
+    /**
+     * 发布消息
+     * */
+    public static void publish(ApplicationEvent event){
+        applicationContext.publishEvent(event);
+        logger.info("发布消息完毕");
     }
 }

758d7ebe4d4543d3bcf3f46b7b7b9d5b2e8e58b0
fanpan26 <fanpan26@126.com>
2017-12-06 11:41:18 +0800

    异常处理

---
+++ src/main/java/com/fyp/layim/web/ErrorController.java
@@ -0,0 +1,18 @@
+package com.fyp.layim.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/error")
+public class ErrorController implements org.springframework.boot.autoconfigure.web.ErrorController{
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+
+    @RequestMapping
+    public String error() {
+        return getErrorPath();
+    }
+}

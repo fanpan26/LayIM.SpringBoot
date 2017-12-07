d20295fc7aea06c56ac1d1c3f56c2ff1e9eb6b6d
fanpan26 <fanpan26@126.com>
2017-12-07 12:30:31 +0800

    申请实体映射

---
+++ src/main/java/com/fyp/layim/domain/mapper/ApplyMapper.java
@@ -0,0 +1,42 @@
+package com.fyp.layim.domain.mapper;
+
+import com.fyp.layim.domain.Apply;
+import com.fyp.layim.domain.ApplyType;
+import com.fyp.layim.domain.viewmodels.ApplyViewModel;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public final class ApplyMapper {
+    public static List<ApplyViewModel> mapApply(List<Apply> applies){
+        if(applies==null || applies.size()==0){
+            return null;
+        }
+        return applies.stream().map(apply -> mapApply(apply)).collect(Collectors.toList());
+    }
+
+    private static ApplyViewModel mapApply(Apply apply){
+        ApplyViewModel viewModel = new ApplyViewModel();
+        viewModel.setId(apply.getId());
+        viewModel.setAvatar(apply.getAvatar());
+        viewModel.setName(apply.getName());
+        viewModel.setContent(getContent(apply.getType(),apply.getRemark()));
+        viewModel.setTime(apply.getCreateAt());
+        viewModel.setUid(apply.getUid());
+        viewModel.setRemark(apply.getRemark());
+        return viewModel;
+    }
+
+    private static String getContent(int type,String remark){
+       if(type == ApplyType.friend){
+           return "申请添加你为好友";
+       }
+       if(type==ApplyType.group){
+           return "申请加入群";
+       }
+       if(type==ApplyType.system){
+            return remark;
+       }
+       return "";
+    }
+}
---
+++ src/main/java/com/fyp/layim/domain/viewmodels/ApplyViewModel.java
@@ -0,0 +1,71 @@
+package com.fyp.layim.domain.viewmodels;
+
+/**
+ * 系统消息展示
+ * */
+public class ApplyViewModel {
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getUid() {
+        return uid;
+    }
+
+    public void setUid(long uid) {
+        this.uid = uid;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    private long uid;
+    private String avatar;
+    private String name;
+    private String content;
+    private String remark;
+    private long time;
+}

62109eb496fb39d7246ba04075040c2d4f1d7d4e
fanpan26 <fanpan26@126.com>
2017-12-13 17:46:22 +0800

    消息状态更新

--- src/main/java/com/fyp/layim/repository/ApplyRepository.java
+++ src/main/java/com/fyp/layim/repository/ApplyRepository.java
@@ -4,7 +4,10 @@ import com.fyp.layim.domain.Apply;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 /**
@@ -27,4 +30,19 @@ public interface ApplyRepository extends JpaRepository<Apply,Long> {
      * 分页获取系统消息
      * */
     Page<Apply> findAppliesByToidOrderByCreateAtDesc(long toId, Pageable pageable);
+
+    /**
+     * 将某个用户的消息设置为已读
+     * */
+    @Transactional
+    @Query(value = "update apply set isread=1 where toid=?1",nativeQuery = true)
+    @Modifying
+    void updateUnreadToRead(long uid);
+
+    /**
+     * 更新申请状态
+     * */
+    @Query(value = "update apply set result=?2 where id=?1",nativeQuery = true)
+    @Modifying
+    void updateResult(long id,int status);
 }

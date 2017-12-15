fbac409267025ed850e501af90a00b13f141063c
fanpan26 <fanpan26@126.com>
2017-12-15 16:45:32 +0800

    重构

--- src/main/java/com/fyp/layim/im/server/handler/LayimMsgHandler.java
+++ src/main/java/com/fyp/layim/im/server/handler/LayimMsgHandler.java
@@ -2,18 +2,23 @@ package com.fyp.layim.im.server.handler;
 
 import com.fyp.layim.im.common.util.SpringUtil;
 import com.fyp.layim.im.packet.LayimContextUserInfo;
+import com.fyp.layim.im.packet.LayimToClientOnlineStatusMsgBody;
+import com.fyp.layim.im.packet.convert.BodyConvert;
 import com.fyp.layim.service.UserService;
 import com.fyp.layim.service.auth.TokenVerify;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.tio.core.Aio;
 import org.tio.core.ChannelContext;
+import org.tio.core.ChannelContextFilter;
 import org.tio.http.common.HttpRequest;
 import org.tio.http.common.HttpResponse;
 import org.tio.http.common.HttpResponseStatus;
 import org.tio.websocket.common.WsRequest;
+import org.tio.websocket.common.WsResponse;
 import org.tio.websocket.server.handler.IWsMsgHandler;
 
+import java.io.IOException;
 import java.net.URLDecoder;
 import java.util.List;
 
@@ -41,6 +46,9 @@ public class LayimMsgHandler implements IWsMsgHandler {
         return handleHandshakeUserInfo(httpRequest,httpResponse,channelContext);
     }
 
+    private UserService getUserService(){
+        return (UserService) SpringUtil.getBean("userService");
+    }
     /**
      * 用 @Autowired 注解UserService 不起作用（原因我不是很清楚）
      * 解析客户端的token，获取用户信息转化为ContextUser对象
@@ -48,7 +56,7 @@ public class LayimMsgHandler implements IWsMsgHandler {
      * 获取用户群组，遍历调用 Aio.bindGroup方法加入群组
      * */
     private HttpResponse handleHandshakeUserInfo(HttpRequest httpRequest, HttpResponse httpResponse, ChannelContext channelContext) throws  Exception {
-        UserService userService = (UserService) SpringUtil.getBean("userService");
+        UserService userService = getUserService();
         //增加token验证方法
 
         String path = httpRequest.getRequestLine().getPath();
@@ -60,8 +68,9 @@ public class LayimMsgHandler implements IWsMsgHandler {
             //没有token 未授权
             httpResponse.setStatus(HttpResponseStatus.C401);
         } else {
+            long uid = Long.parseLong(userId);
             //解析token
-            LayimContextUserInfo userInfo = userService.getContextUserInfo(Long.parseLong(userId));
+            LayimContextUserInfo userInfo = userService.getContextUserInfo(uid);
             if (userInfo == null) {
                 //没有找到用户
                 httpResponse.setStatus(HttpResponseStatus.C404);
@@ -73,15 +82,41 @@ public class LayimMsgHandler implements IWsMsgHandler {
                 List<String> groupIds = userInfo.getGroupIds();
                 //绑定用户群信息
                 if (groupIds != null) {
-                    groupIds.forEach((String groupId) -> Aio.bindGroup(channelContext, groupId));
+                    groupIds.forEach(groupId -> Aio.bindGroup(channelContext, groupId));
                 }
+                //通知所有好友本人上线了
+                notify(channelContext,true);
             }
         }
         return httpResponse;
     }
 
+    /**
+     * 通知该用户的好友上线消息
+     * */
+    private void notify(ChannelContext channelContext,boolean online) throws IOException{
+        long uid = Long.parseLong(channelContext.getUserid());
+        List<String> allFriendIds = getUserService().getAllFriends(uid);
+        if(allFriendIds.size()==0){
+            return;
+        }
+        LayimToClientOnlineStatusMsgBody msgBody = new LayimToClientOnlineStatusMsgBody(uid,online);
+        WsResponse statusPacket = BodyConvert.getInstance().convertToTextResponse(msgBody);
+
+        Aio.sendToAll(channelContext.getGroupContext(), statusPacket, filterChannelContext -> {
+            if(filterChannelContext.isRemoved()||filterChannelContext.isClosed()) {
+                return false;
+            }
+            String channelContextUserid = filterChannelContext.getUserid();
+            boolean exists = allFriendIds.stream().anyMatch(friendUserId -> friendUserId.equals(channelContextUserid));
+            return exists;
+        });
+    }
+
     @Override
     public Object onClose(WsRequest wsRequest, byte[] bytes, ChannelContext channelContext) throws Exception {
+        notify(channelContext,false);
+        Aio.remove(channelContext,"onClose");
         return null;
     }
 }

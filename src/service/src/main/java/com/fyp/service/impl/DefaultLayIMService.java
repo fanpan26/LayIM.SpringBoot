4b8e59438cb2cda941dff961886a6dbb9f5c50c6
fanpan26 <fanpan26@126.com>
2018-12-24 15:46:01 +0800

    session.close

--- src/service/src/main/java/com/fyp/service/impl/DefaultLayIMService.java
+++ src/service/src/main/java/com/fyp/service/impl/DefaultLayIMService.java
@@ -21,45 +21,49 @@ public class DefaultLayIMService implements LayIMService {
     public JsonResult GetInitResult(Long userId) {
 
         SqlSession session = MyBatisUtil.getSession();
-        UserMapper userMapper = session .getMapper(UserMapper.class);
-        FriendGroupMapper friendGroupMapper = session.getMapper(FriendGroupMapper.class);
-        UserFriendGroupMapper userFriendGroupMapper = session.getMapper(UserFriendGroupMapper.class);
-        BigGroupMapper bigGroupMapper = session.getMapper(BigGroupMapper.class);
+        try {
+            UserMapper userMapper = session.getMapper(UserMapper.class);
+            FriendGroupMapper friendGroupMapper = session.getMapper(FriendGroupMapper.class);
+            UserFriendGroupMapper userFriendGroupMapper = session.getMapper(UserFriendGroupMapper.class);
+            BigGroupMapper bigGroupMapper = session.getMapper(BigGroupMapper.class);
 
-        InitResult initResult = new InitResult();
-        initResult.setMine(userMapper.getUser(userId));
-
-        List<FriendGroup> friendGroups = friendGroupMapper.getGroupsByUserId(userId);
-        if(!friendGroups.isEmpty()) {
-            List<Long> friendGroupIds = friendGroups
-                    .stream()
-                    .map(x -> x.getId())
-                    .collect(Collectors.toList());
-
-            List<UserFriendGroup> userFriendGroups = userFriendGroupMapper.getUserIdsByGroupIds(friendGroupIds);
-            //根据所有的用户ID查询出用户信息
-            if(!userFriendGroups.isEmpty()) {
-                List<Long> friendIds = userFriendGroups
+            InitResult initResult = new InitResult();
+            initResult.setMine(userMapper.getUser(userId));
+            List<FriendGroup> friendGroups = friendGroupMapper.getGroupsByUserId(userId);
+            if (!friendGroups.isEmpty()) {
+                List<Long> friendGroupIds = friendGroups
                         .stream()
-                        .map(x -> x.getUserId())
+                        .map(x -> x.getId())
                         .collect(Collectors.toList());
-                List<User> friends = userMapper.getUsersByIds(friendIds);
 
-                friendGroups.stream().forEach(x -> {
-                     List<Long> userIdInGroup = userFriendGroups.stream()
-                             .filter(u -> u.getGroupId() == x.getId())
-                             .map(m -> m.getUserId())
-                             .collect(Collectors.toList());
+                List<UserFriendGroup> userFriendGroups = userFriendGroupMapper.getUserIdsByGroupIds(friendGroupIds);
+                //根据所有的用户ID查询出用户信息
+                if (!userFriendGroups.isEmpty()) {
+                    List<Long> friendIds = userFriendGroups
+                            .stream()
+                            .map(x -> x.getUserId())
+                            .collect(Collectors.toList());
+                    List<User> friends = userMapper.getUsersByIds(friendIds);
 
-                     x.setList(friends.stream()
-                             .filter(f -> userIdInGroup.contains(f.getId()))
-                             .collect(Collectors.toList()));
-                });
+                    friendGroups.stream().forEach(x -> {
+                        List<Long> userIdInGroup = userFriendGroups.stream()
+                                .filter(u -> u.getGroupId() == x.getId())
+                                .map(m -> m.getUserId())
+                                .collect(Collectors.toList());
+
+                        x.setList(friends.stream()
+                                .filter(f -> userIdInGroup.contains(f.getId()))
+                                .collect(Collectors.toList()));
+                    });
+                }
+
+                initResult.setFriend(friendGroups);
+                initResult.setGroup(bigGroupMapper.getGroupsByUserId(userId));
             }
 
-            initResult.setFriend(friendGroups);
-            initResult.setGroup(bigGroupMapper.getGroupsByUserId(userId));
+            return JsonResult.success(initResult);
+        } finally {
+            session.close();
         }
-        return JsonResult.success(initResult);
     }
 }

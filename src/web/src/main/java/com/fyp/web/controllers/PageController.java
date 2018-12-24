add11b1da91b727309c74b0e6db84c23728aa461
fanpan26 <fanpan26@126.com>
2018-12-24 10:57:46 +0800

    add html files

---
+++ src/web/src/main/java/com/fyp/web/controllers/PageController.java
@@ -0,0 +1,13 @@
+package com.fyp.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}

a36fe163766638e75d50c45732b673983933dfa5
fanpan26 <fanpan26@126.com>
2019-01-26 15:04:12 +0800

    Update Utils.java
    如果是群组，使用群组ID，否则使用两个ID连起来

--- src/service/src/main/java/com/fyp/service/utils/Utils.java
+++ src/service/src/main/java/com/fyp/service/utils/Utils.java
@@ -3,9 +3,9 @@ package com.fyp.service.utils;
 public class Utils {
     /**
      * 生成房间ID
-     * */
+     */
     public static long generateRoomId(long from, long to, String msgType) {
-        if ("group".equals(msgType)) {
+        if (!"group".equals(msgType)) {
             if (from > to) {
                 return Long.valueOf(from + "" + to);
             }

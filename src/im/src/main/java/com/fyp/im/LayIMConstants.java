0bf7c5796da150b8dd128760e0fee9dd5f0319d2
fanpan26 <fanpan26@126.com>
2019-01-30 18:14:06 +0800

    constants

---
+++ src/im/src/main/java/com/fyp/im/LayIMConstants.java
@@ -0,0 +1,12 @@
+package com.fyp.im;
+
+public final class LayIMConstants {
+    /**
+     * LayIMService 标识
+     * */
+    public static final String LAYIM_SERVICE = "layimService";
+    /**
+     * ChannelContext setAttribute(CURRENT_USER_ATTRIBUTE,userInfo)
+     * */
+    public static final String CURRENT_USER_ATTRIBUTE = "current_user";
+}

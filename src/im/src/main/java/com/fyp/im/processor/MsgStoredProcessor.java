7822c52417e1e3493c90fe477ebfa2edaf46dc4c
fanpan26 <fanpan26@126.com>
2019-01-30 18:15:04 +0800

    add buildSendMessage

--- src/im/src/main/java/com/fyp/im/processor/MsgStoredProcessor.java
+++ src/im/src/main/java/com/fyp/im/processor/MsgStoredProcessor.java
@@ -1,11 +1,16 @@
 package com.fyp.im.processor;
 
 import com.fyp.entity.MsgRecord;
+import com.fyp.entity.User;
+import com.fyp.entity.im.Message;
+import com.fyp.im.LayIMConstants;
+import com.fyp.im.MsgType;
 import com.fyp.im.utils.BeanUtil;
+import com.fyp.im.utils.SafeEncoder;
 import com.fyp.service.intf.LayIMService;
 import org.tio.core.ChannelContext;
+import org.tio.utils.json.Json;
 
-import java.io.UnsupportedEncodingException;
 import java.util.concurrent.Executors;
 
 public abstract class MsgStoredProcessor extends AbstractMsgProcessor{
@@ -13,24 +18,35 @@ public abstract class MsgStoredProcessor extends AbstractMsgProcessor{
     private void recordMsgAsync(ChannelContext channelContext){
         Executors.newCachedThreadPool().execute(() -> recordMsg(channelContext));
     }
-    private void recordMsg(ChannelContext channelContext) {
-        byte[] body = getBody();
-        String msg;
-        try {
-            msg = new String(body, "utf-8");
-        } catch (UnsupportedEncodingException e) {
-            return;
+
+    protected final byte[] buildSendMessage(ChannelContext channelContext) {
+        Message message = new Message();
+
+        User user = (User) channelContext.getAttribute(LayIMConstants.CURRENT_USER_ATTRIBUTE);
+        if(null == user){
+            throw new NullPointerException("current user is null");
         }
+        message.setUsername(user.getUsername());
+        message.setAvatar(user.getAvatar());
+        message.setContent(SafeEncoder.encode(getBody()));
+        message.setType(getType(channelContext));
+        message.setId(getId(channelContext));
+        return SafeEncoder.encode(Json.toJson(message));
+    }
+
+    protected abstract long getId(ChannelContext channelContext);
+    protected abstract String getType(ChannelContext channelContext);
+
+    private void recordMsg(ChannelContext channelContext) {
         LayIMService service = ((LayIMService) BeanUtil.getBean("layIMService"));
         MsgRecord record = new MsgRecord();
         record.setTo(getTargetId());
         record.setFrom(Long.valueOf(channelContext.userid));
-        record.setContents(msg);
+        record.setContents(SafeEncoder.encode(getBody()));
         record.setCreateAt(System.currentTimeMillis());
         record.setId(System.currentTimeMillis());
         service.addRecord(record);
     }
-
 
     protected abstract void processMessage(ChannelContext channelContext);
 

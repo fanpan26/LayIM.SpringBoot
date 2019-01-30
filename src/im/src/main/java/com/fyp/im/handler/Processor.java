3f2b0110a799aa56bb067c922d5fa59696a15f7f
fanpan26 <fanpan26@126.com>
2019-01-30 18:13:50 +0800

    create new processor ,not singleton

--- src/im/src/main/java/com/fyp/im/handler/Processor.java
+++ src/im/src/main/java/com/fyp/im/handler/Processor.java
@@ -4,32 +4,30 @@ import com.fyp.im.MsgType;
 import com.fyp.im.processor.AbstractMsgProcessor;
 import com.fyp.im.processor.ClientToClientProcessor;
 import com.fyp.im.processor.ClientToGroupProcessor;
+import org.springframework.context.NoSuchMessageException;
 import org.tio.core.ChannelContext;
 
+import java.security.NoSuchProviderException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Processor {
     /**
      * 消息处理入口
-     * */
-    public static Object process(ChannelContext channelContext,byte[] body) {
+     */
+    public static Object process(ChannelContext channelContext, byte[] body) {
         getProcessor(body).process(channelContext, body);
         return null;
     }
 
     private static AbstractMsgProcessor getProcessor(byte[] body) {
         MsgType type = MsgType.valueOf(body[4]);
-        AbstractMsgProcessor processor = processors.get(type);
-        if (processor == null) {
-            throw new IllegalArgumentException("no msgProcessor for msgType :" + type.getValue());
+        switch (type) {
+            case clientToClient:
+                return new ClientToClientProcessor();
+            case clientToGroup:
+                return new ClientToClientProcessor();
         }
-        return processor;
-    }
-    private static final Map<MsgType,AbstractMsgProcessor> processors = new HashMap<>();
-
-    static {
-        processors.put(MsgType.clientToClient,new ClientToClientProcessor());
-        processors.put(MsgType.clientToGroup,new ClientToGroupProcessor());
+        return null;
     }
 }

f674b345322aa8c799b7b065bbf49910ed406d32
fanpan26 <fanpan26@126.com>
2019-01-30 18:14:21 +0800

    body cache

--- src/im/src/main/java/com/fyp/im/processor/AbstractMsgProcessor.java
+++ src/im/src/main/java/com/fyp/im/processor/AbstractMsgProcessor.java
@@ -1,6 +1,5 @@
 package com.fyp.im.processor;
 
-import com.fyp.im.MsgType;
 import com.fyp.utils.convert.ConvertUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,13 +12,16 @@ public abstract class AbstractMsgProcessor {
     private static final Logger logger = LoggerFactory.getLogger(AbstractMsgProcessor.class);
     private byte[] body;
     private static final int PREFIX_LENGTH = 5;
+    private static final  int BODY_LENGTH_LENGTH = 4;
+    private byte[] realBody;
+    private int targetId;
 
     public AbstractMsgProcessor() {
 
     }
 
     private void setBody(byte[] body) {
-        if (body == null || body.length < 5) {
+        if (body == null || body.length < 9) {
             throw new IllegalArgumentException("illegal msg body");
         }
         this.body = body;
@@ -28,7 +30,7 @@ public abstract class AbstractMsgProcessor {
     /**
      * 消息处理入口
      * */
-    public void process(ChannelContext channelContext, byte[] body) {
+    public final void process(ChannelContext channelContext, byte[] body) {
         if(logger.isDebugEnabled()){
             logger.debug("handle msg:{}",new String(body));
         }
@@ -41,21 +43,28 @@ public abstract class AbstractMsgProcessor {
     /**
      * 获取接收人的ID
      */
-    protected int getTargetId() {
-        byte[] targetIdBytes = Arrays.copyOf(body, 4);
-        return ConvertUtil.byteArrayToInt(targetIdBytes);
+    protected final int getTargetId() {
+        if (targetId == 0) {
+            byte[] targetIdBytes = Arrays.copyOf(body, 4);
+            targetId = ConvertUtil.byteArrayToInt(targetIdBytes);
+        }
+        return targetId;
     }
 
-    /**
-     * 获取消息类型
-     */
-    protected MsgType getMsgType() {
-        byte value = body[4];
-        return MsgType.valueOf(value);
+    protected final int getBodyLength() {
+        byte[] bodyLengthBytes =  Arrays.copyOfRange(body, PREFIX_LENGTH, PREFIX_LENGTH + BODY_LENGTH_LENGTH);
+        return ConvertUtil.byteArrayToInt(bodyLengthBytes);
     }
 
-    protected byte[] getBody() {
-        byte[] contents = Arrays.copyOfRange(body, PREFIX_LENGTH, body.length);
-        return contents;
+    protected final byte[] getBody() {
+        if (realBody == null) {
+            int length = getBodyLength();
+            int totalLength = PREFIX_LENGTH + BODY_LENGTH_LENGTH + length;
+            if (body.length != totalLength) {
+                throw new IllegalArgumentException("illegal body length");
+            }
+            realBody = Arrays.copyOfRange(body, PREFIX_LENGTH + BODY_LENGTH_LENGTH, totalLength);
+        }
+        return realBody;
     }
 }

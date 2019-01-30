51710af90352233bd87f298d3da524aaf57d2675
fanpan26 <fanpan26@126.com>
2019-01-30 18:15:55 +0800

    add GetByUserId

--- src/service/src/main/java/com/fyp/service/intf/LayIMService.java
+++ src/service/src/main/java/com/fyp/service/intf/LayIMService.java
@@ -1,11 +1,14 @@
 package com.fyp.service.intf;
 
 import com.fyp.entity.MsgRecord;
+import com.fyp.entity.User;
 import com.fyp.entity.result.JsonResult;
 
 import java.util.List;
 
 public interface LayIMService {
+    User GetByUserId(Long userId);
+
     JsonResult GetInitResult(Long userId);
 
     JsonResult GetMembersByGroupId(Long groupId);

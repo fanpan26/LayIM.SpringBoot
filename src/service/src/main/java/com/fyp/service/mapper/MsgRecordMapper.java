f58f56ddc65bb8f5cfe0042fd4c138a37fde50ac
fanpan26 <fanpan26@126.com>
2019-02-01 12:05:15 +0800

    add getList method

--- src/service/src/main/java/com/fyp/service/mapper/MsgRecordMapper.java
+++ src/service/src/main/java/com/fyp/service/mapper/MsgRecordMapper.java
@@ -2,6 +2,10 @@ package com.fyp.service.mapper;
 
 import com.fyp.entity.MsgRecord;
 
+import java.util.List;
+
 public interface MsgRecordMapper {
     void addRecord(MsgRecord record);
+
+    List<MsgRecord> getList(Long roomId,long lastId);
 }

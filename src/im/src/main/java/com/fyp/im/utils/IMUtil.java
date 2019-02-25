eb16935f64f82aac9d9ed7f8fb8a20b8c0ed3267
fanpan26 <fanpan26@126.com>
2019-02-25 10:13:42 +0800

    rename

--- src/im/src/main/java/com/fyp/im/utils/IMUtil.java
+++ src/im/src/main/java/com/fyp/im/utils/IMUtil.java
@@ -7,16 +7,16 @@ import org.tio.websocket.common.WsResponse;
 
 public class IMUtil {
 
-    private static WsResponse buildReponse(byte[] body) {
+    private static WsResponse buildResponse(byte[] body) {
         return WsResponse.fromBytes(body);
     }
 
     public static void sendToGroup(ChannelContext channelContext, long targetId, byte[] body) {
         String currentUserId = channelContext.userid;
-        Tio.sendToGroup(channelContext.getGroupContext(), String.valueOf(targetId), buildReponse(body), ctx -> !ctx.userid.equals(String.valueOf(currentUserId)));
+        Tio.sendToGroup(channelContext.getGroupContext(), String.valueOf(targetId), buildResponse(body), ctx -> !ctx.userid.equals(currentUserId));
     }
 
     public static void send(ChannelContext channelContext, long targetId, byte[] body) {
-        Tio.sendToUser(channelContext.getGroupContext(), String.valueOf(targetId), buildReponse(body));
+        Tio.sendToUser(channelContext.getGroupContext(), String.valueOf(targetId), buildResponse(body));
     }
 }
